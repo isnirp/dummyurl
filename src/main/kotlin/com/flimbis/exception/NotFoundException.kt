9a1f4dbf24057a2092f8469220e44d1cfd2c99b9
isnirp <pramankwah@gmail.com>
2023-02-21 15:00:06 +0100

    define default constructor for notfoundexception

--- src/main/kotlin/com/flimbis/exception/NotFoundException.kt
+++ src/main/kotlin/com/flimbis/exception/NotFoundException.kt
@@ -5,5 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus
 import java.lang.RuntimeException
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-class NotFoundException(msg: String): RuntimeException(msg) {
+class NotFoundException : RuntimeException {
+    constructor() : super()
+    constructor(msg: String) : super(msg)
 }
