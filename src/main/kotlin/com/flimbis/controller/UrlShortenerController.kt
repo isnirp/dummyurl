5717dbc9b98083cacad94492d418f62fdc896831
isnirp <pramankwah@gmail.com>
2023-02-22 13:54:14 +0100

    redirect

--- src/main/kotlin/com/flimbis/controller/UrlShortenerController.kt
+++ src/main/kotlin/com/flimbis/controller/UrlShortenerController.kt
@@ -2,7 +2,9 @@ package com.flimbis.controller
 
 import com.flimbis.model.UrlDto
 import com.flimbis.service.UrlShortenerService
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -10,8 +12,9 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import java.net.URI
 
-const val BASE_URL = "http://localhost:8081/"
+const val BASE_URL = "http://localhost:8081/api/"
 
 @RestController
 @RequestMapping("/api")
@@ -28,5 +31,15 @@ class UrlShortenerController(private val service: UrlShortenerService) {
     fun retrieveOriginalUrl(@PathVariable shortPath: String): UrlDto {
         val url = service.getOriginalUrl(shortPath)
         return UrlDto(url!!)
+    }
+
+    @GetMapping("/{shortPath}")
+    fun redirectUrl(@PathVariable shortPath: String): ResponseEntity<HttpStatus> {
+        val url = service.getOriginalUrl(shortPath)
+        return ResponseEntity
+            .status(HttpStatus.MOVED_PERMANENTLY)
+            .location(URI.create(url!!))
+            .header(HttpHeaders.CONNECTION, "close")
+            .build()
     }
 }
